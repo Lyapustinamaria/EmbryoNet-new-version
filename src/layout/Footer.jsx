9dbebaf0d95b5c13cb7a4463b22ab2928c00a15e
Mariia Liapustina <lyapustinamaria@mail.ru>
2023-06-27 21:27:27 +0200

    Add Footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,3 +1,86 @@
-function Footer() {}
+function Footer() {
+    return (
+        <footer className="bg-light text-center text-white">
+            {/* Grid container */}
+            <div className="container p-4 pb-0">
+                {/* Section: Social media */}
+                <section className="mb-4">
+                    {/* Facebook */}
+                    <a
+                        className="btn text-white btn-floating m-1"
+                        style={{ backgroundColor: '#3b5998' }}
+                        href="#!"
+                        role="button"
+                    >
+                        <i className="bi bi-facebook"></i>
+                    </a>
+
+                    {/* Twitter */}
+                    <a
+                        className="btn text-white btn-floating m-1"
+                        style={{ backgroundColor: '#55acee' }}
+                        href="#!"
+                        role="button"
+                    >
+                        <i className="bi bi-twitter"></i>
+                    </a>
+
+                    {/* Google */}
+                    <a
+                        className="btn text-white btn-floating m-1"
+                        style={{ backgroundColor: '#dd4b39' }}
+                        href="#!"
+                        role="button"
+                    >
+                        <i className="bi bi-google"></i>
+                    </a>
+
+                    {/* Instagram */}
+                    <a
+                        className="btn text-white btn-floating m-1"
+                        style={{ backgroundColor: '#ac2bac' }}
+                        href="#!"
+                        role="button"
+                    >
+                        <i className="bi bi-instagram"></i>
+                    </a>
+
+                    {/* Linkedin */}
+                    <a
+                        className="btn text-white btn-floating m-1"
+                        style={{ backgroundColor: '#0082ca' }}
+                        href="#!"
+                        role="button"
+                    >
+                        <i className="bi bi-linkedin"></i>
+                    </a>
+                    {/* Github */}
+                    <a
+                        className="btn text-white btn-floating m-1"
+                        style={{ backgroundColor: '#333333' }}
+                        href="#!"
+                        role="button"
+                    >
+                        <i className="bi bi-github"></i>
+                    </a>
+                </section>
+                {/* Section: Social media */}
+            </div>
+            {/* Grid container */}
+
+            {/* Copyright */}
+            <div
+                className="text-center p-3"
+                style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+            >
+                © 2023 Copyright:
+                <a className="text-white" href="https://mdbootstrap.com/">
+                    Computer Vision Studio UG. All rights reserved.
+                </a>
+            </div>
+            {/* Copyright */}
+        </footer>
+    );
+}
 
 export { Footer };
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,15 @@
 function Header() {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary" color="gray-100">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    Navbar
+        <nav
+            className="navbar navbar-expand-lg bg-body-tertiary"
+            color="gray-100"
+        >
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    EmbryoNet
                 </a>
                 <button
-                    class="navbar-toggler"
+                    className="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent"
@@ -14,30 +17,30 @@ function Header() {
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div
-                    class="collapse navbar-collapse"
+                    className="collapse navbar-collapse"
                     id="navbarSupportedContent"
                 >
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
                             <a
-                                class="nav-link active"
+                                className="nav-link active"
                                 aria-current="page"
                                 href="#"
                             >
                                 Home
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
                                 Link
                             </a>
                         </li>
-                        <li class="nav-item dropdown">
+                        <li className="nav-item dropdown">
                             <a
-                                class="nav-link dropdown-toggle"
+                                className="nav-link dropdown-toggle"
                                 href="#"
                                 role="button"
                                 data-bs-toggle="dropdown"
@@ -45,39 +48,42 @@ function Header() {
                             >
                                 Dropdown
                             </a>
-                            <ul class="dropdown-menu">
+                            <ul className="dropdown-menu">
                                 <li>
-                                    <a class="dropdown-item" href="#">
+                                    <a className="dropdown-item" href="#">
                                         Action
                                     </a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="#">
+                                    <a className="dropdown-item" href="#">
                                         Another action
                                     </a>
                                 </li>
                                 <li>
-                                    <hr class="dropdown-divider" />
+                                    <hr className="dropdown-divider" />
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="#">
+                                    <a className="dropdown-item" href="#">
                                         Something else here
                                     </a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
+                        <li className="nav-item">
+                            <a className="nav-link disabled">Disabled</a>
                         </li>
                     </ul>
-                    <form class="d-flex" role="search">
+                    <form className="d-flex" role="search">
                         <input
-                            class="form-control me-2"
+                            className="form-control me-2"
                             type="search"
                             placeholder="Search"
                             aria-label="Search"
                         />
-                        <button class="btn btn-outline-success" type="submit">
+                        <button
+                            className="btn btn-outline-success"
+                            type="submit"
+                        >
                             Search
                         </button>
                     </form>
